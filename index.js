c1d6935b322de17feef1482c37730b4b79ebc7b6
Eric Teixeira <ertfly@gmail.com>
2023-03-20 10:06:21 -0300

    add custom oauth2

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 
 const swaggerUi = require('swagger-ui-express'),
-swaggerDocument = require('./swagger.json');
-
+  swaggerDocument = require('./swagger.json');
 
 app.use(
   '/api-docs',
-  swaggerUi.serve, 
-  swaggerUi.setup(swaggerDocument)
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      docExpansions: "none",
+      persistAuthorization: true
+    }
+  })
 );
 
 app.listen(8001, () => {
